50e8c430a2147881896fb450a8eee0b34f679d95
junseop <baalftgg@gmail.com>
2023-11-08 09:41:25 +0900

    tempo 숫자 입력 방식 변경, 오디오 재생 및 다운로드 기능 추가

--- src/App.js
+++ src/App.js
@@ -17,7 +17,10 @@ function App() {
   const [audioValue, setAudioValue] = useState(null);
   
   const createTTS = () => {
-    const query = 'text=${text}&master_ket=${master_key}&call_id=${call_id}&sid=${sid}&tempo=${tempo}&sample_rate=${sample_rate}';
+    setAudioValue(null);
+    setTTS(false);
+
+    const query = `text=${text}&master_key=${master_key}&call_id=${call_id}&sid=${sid}&tempo=${tempo}&sample_rate=${sample_rate}`;
 
     fetch('http://192.168.220.224:26000/ttsstream?' + query, {method: 'GET', mode: 'cors'})
     .then((response) => response.blob())
@@ -46,20 +49,51 @@ function App() {
           <option value="option1">Korean</option>
        </select>
         <select className='body-select-two' value={sid} onChange={(e) => setSid(parseInt(e.target.value))}>
-          <option value={0}>옵션 A</option>
-          <option value={1}>옵션 B</option>
-          <option value={2}>옵션 C</option>
+          <option value={0}>김하윤(일반)</option>
+          <option value={1}>김하윤(밝은)</option>
+          <option value={2}>한보람(일반)</option>
+          <option value={3}>한보람(밝은)</option>
+          <option value={4}>차유나(일반)</option>
+          <option value={5}>차유나(밝은)</option>
+          <option value={6}>노경연(일반)</option>
+          <option value={7}>노경연(밝은)</option>
+          <option value={8}>연빛나(일반)</option>
+          <option value={9}>연빛나(밝은)</option>
+          <option value={10}>김동희(일반)</option>
+          <option value={11}>김동희(밝은)</option>
+          <option value={12}>오세혁(일반)</option>
+          <option value={13}>오세혁(밝은)</option>
+          <option value={14}>조용민</option>
+          <option value={15}>김경일</option>
+          <option value={16}>김상균</option>
+          <option value={17}>김완선</option>
+          <option value={18}>이경일</option>
+          <option value={19}>카이스트</option>
+          <option value={20}>지향</option>
+          <option value={21}>세하</option>
+          <option value={22}>토니</option>
+          <option value={23}>엘리</option>
+          <option value={24}>로이</option>
+          <option value={25}>이든</option>
+          <option value={26}>루디</option>
+          <option value={27}>노아</option>
+          <option value={28}>루비</option>
+          <option value={29}>세미</option>
+          <option value={30}>제니</option>
+          <option value={31}>로운</option>
+          <option value={32}>이안</option>
+          <option value={33}>조이</option>
       </select>
-      <select className='body-select-three' value={tempo} onChange={(e) => setTempo(parseFloat(e.target.value))}>
-        <option value={0.25}>0.25x</option>
-        <option value={0.5}>0.5x</option>
-        <option value={0.75}>0.75x</option>
-        <option value={1}>1x</option>
-        <option value={1.25}>1.25x</option>
-        <option value={1.5}>1.5x</option>
-        <option value={1.75}>1.75x</option>
-        <option value={2}>2x</option>
-      </select>
+      <input 
+        type="number" 
+        step="0.01" 
+        value={tempo} 
+        onChange={(e) => {
+          const inputValue = parseFloat(e.target.value);
+          const roundedValue = Math.round(inputValue * 100) / 100;
+          setTempo(roundedValue);
+        }}
+      />
         </div>
         <div className='body-input-box'>
         <textarea 
@@ -74,13 +108,12 @@ function App() {
           <button className='body-btn' onClick={createTTS}>generate audio</button>
           {tts && audioValue && (
             <audio controls>
-              <source src={'data:audio/wav;base64,${audioValue}'} type='audio/wav' />
+              <source src={`data:audio/wav;base64,${audioValue}`} type='audio/wav' />
             </audio>
           )}
         </div>
       </body>
     </div>
-
   );
 }
 
